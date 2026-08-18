05ed8a5eae3b8d12d0901a41b6f147ce3c454d50
pd-phuc <phamducphucdn@gmail.com>
2026-08-18 22:06:56 +0700

    feat: setup Storybook for testing

--- apps/web/eslint.config.ts
+++ apps/web/eslint.config.ts
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
 import rootConfig from '../../eslint.config';
 
-export default [...rootConfig];
+export default [...rootConfig, ...storybook.configs['flat/recommended']];
